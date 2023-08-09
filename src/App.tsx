bb22228c46865ba8ff6c50cf8ff546da49acaa92
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2023-08-09 16:31:30 +0800

    feat：增加按钮样式

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Button disabled>Hello Button1</Button>
-        <Button btnType={ ButtonType.Primary } size={ ButtonSize.Large }>Hello Button2</Button>
-        <Button btnType={ ButtonType.Link } href="http://baidu.com" disabled>Hello Button3</Button>
+        <Button btnType={ ButtonType.Primary } size={ ButtonSize.Large }>Hello Button2</Button>  
+        <Button btnType={ ButtonType.Danger }>Hello Button3</Button>
+        <Button btnType={ ButtonType.Link } href="http://baidu.com" disabled>Hello Button4</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
