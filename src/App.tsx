0f59376f9a3b32b3e6c57f2d846af776b27feaaf
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2023-08-15 14:53:00 +0800

    update：完善按钮组件

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button className="custom" onClick={(e) => { e.preventDefault(); alert(123); }}>Hello Button</Button>
         <Button disabled>Hello Button1</Button>
         <Button btnType={ ButtonType.Primary } size={ ButtonSize.Large }>Hello Button2</Button>  
         <Button btnType={ ButtonType.Danger }>Hello Button3</Button>
         <Button btnType={ ButtonType.Link } href="http://baidu.com" disabled>Hello Button4</Button>
+        <Button btnType={ ButtonType.Link } href="http://baidu.com" target='_blank'>Hello Button5</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -22,16 +22,22 @@ interface BaseButtonProps {
   href?: string;
 };
 
-const Button: React.FC<BaseButtonProps> = (props) => {
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+
+const Button: React.FC<ButtonProps> = (props) => {
   const {
     btnType,
+    className,
     disabled,
     size,
     children,
     href,
+    ...restProps
   } = props;
 
-  const classes = classNames("btn", {
+  const classes = classNames("btn", className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     "disabled": (btnType === ButtonType.Link) && disabled,
@@ -43,6 +49,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
       <a
         className={ classes }
         href={ href }
+        { ...restProps }
       >
         { children }
       </a>
@@ -52,6 +59,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
       <button
         className={ classes }
         disabled={ disabled }
+        { ...restProps }
       >
         { children }
       </button>
