1255db3f7f51702c79a2af3f973262f8d6cd432b
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2023-10-29 16:26:32 +0800

    feat：新增Button组件单元测试

---
+++ src/components/Button/button.test.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button, { ButtonProps, ButtonType, ButtonSize } from './button';
+
+const defaultProps = {
+  onClick: jest.fn(),
+};
+
+const testProps: ButtonProps = {
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  className: 'test-class',
+};
+
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn(),
+};
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const view = render(<Button {...defaultProps}>Nice</Button>);
+    const element = view.getByText('Nice') as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('BUTTON');
+    expect(element).toHaveClass('btn btn-default');
+    expect(element.disabled).toBeFalsy();
+    fireEvent.click(element);
+    expect(defaultProps.onClick).toHaveBeenCalled();
+  });
+  it('should render the correct component based on different props', () => {
+    const view = render(<Button {...testProps}>Nice</Button>);
+    const element = view.getByText('Nice');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass('btn-primary btn-lg test-class');
+  });
+  it('should render a link when btnType equals link and href is provided', () => {
+    const view = render(<Button btnType={ ButtonType.Link } href="http://dummyurl">Link</Button>);
+    const element = view.getByText('Link');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('A');
+    expect(element).toHaveClass('btn btn-link');
+  });
+  it('should render disabled button when disabled set to true', () => {
+    const view = render(<Button { ...disabledProps }>Nice</Button>);
+    const element = view.getByText('Nice') as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.disabled).toBeTruthy();
+    fireEvent.click(element);
+    expect(disabledProps.onClick).not.toHaveBeenCalled();
+  });
+});
