f1bb5941bbbc66f0abf1ef52a30f257cbaa4d15f
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2024-01-07 15:35:15 +0800

    update：更新Menu组件单元测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react';
-import Menu, { MenuProps } from './menu';
+import { render, RenderResult, fireEvent, waitFor } from '@testing-library/react';
+import Menu, {MenuProps} from './menu';
 import MenuItem from './menuItem';
+import SubMenu from './subMenu';
 
 const testProps: MenuProps = {
   defaultIndex: '0',
   onSelect: jest.fn(),
   className: 'test',
-};
-
+}
 const testVerProps: MenuProps = {
   defaultIndex: '0',
   mode: 'vertical',
-};
-
+  defaultOpenSubMenus: ['4'],
+}
 const generateMenu = (props: MenuProps) => {
   return (
     <Menu {...props}>
@@ -24,17 +24,41 @@ const generateMenu = (props: MenuProps) => {
         disabled
       </MenuItem>
       <MenuItem>
-        better
+        xyz
       </MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>
+          drop1
+        </MenuItem>
+      </SubMenu>
+      <SubMenu title="opened">
+        <MenuItem>
+          opened1
+        </MenuItem>
+      </SubMenu>
     </Menu>
-  );
-};
-
+  )
+}
+const createStyleFile = () => {
+  const cssFile: string = `
+    .better-submenu {
+      display: none;
+    }
+    .better-submenu.menu-opened {
+      display:block;
+    }
+  `;
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = cssFile;
+  return style;
+}
 let screen: RenderResult, menuElement: HTMLElement, activeElement: HTMLElement, disabledElement: HTMLElement;
-describe('test Menu and MenuItem component', () => {
+describe('test Menu and MenuItem component in default(horizontal) mode', () => {
   beforeEach(() => {
     // eslint-disable-next-line testing-library/no-render-in-setup
     screen = render(generateMenu(testProps));
+    screen.container.append(createStyleFile());
     menuElement= screen.getByTestId('test-menu');
     activeElement = screen.getByText('active');
     disabledElement = screen.getByText('disabled');
@@ -43,25 +67,49 @@ describe('test Menu and MenuItem component', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('better-menu test');
     // eslint-disable-next-line testing-library/no-node-access
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(5);
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
   it('click items should change active and call the right callback', () => {
-    const thirdItem = screen.getByText('better');
+    const thirdItem = screen.getByText('xyz');
     fireEvent.click(thirdItem);
     expect(thirdItem).toHaveClass('is-active');
     expect(activeElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).toHaveBeenCalledWith(2);
+    expect(testProps.onSelect).toHaveBeenCalledWith('2');
     fireEvent.click(disabledElement);
     expect(disabledElement).not.toHaveClass('is-active');
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
+    expect(testProps.onSelect).not.toHaveBeenCalledWith('1');
+  });
+  it('should show dropdown items when click on subMenu', async () => {
+    expect(screen.queryByText('drop1')).not.toBeVisible();
+    const dropdownElement = screen.getByText('dropdown');
+    fireEvent.click(dropdownElement);
+    expect(screen.queryByText('drop1')).toBeVisible();
+  })
+})
+describe('test Menu and MenuItem component in vertical mode', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    screen = render(generateMenu(testVerProps));
+    screen.container.append(createStyleFile());
   });
   it('should render vertical mode when mode is set to vertical', () => {
-    cleanup();
-    // eslint-disable-next-line testing-library/render-result-naming-convention
-    const screen = render(generateMenu(testVerProps));
     const menuElement = screen.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical');
-  })
+  });
+  it('should show subMenu dropdown when defaultOpenSubMenus contains SubMenu index', () => {
+    expect(screen.queryByText('opened1')).toBeVisible();
+  });
+  it('should show dropdown items when hover on subMenu for vertical mode', async () => {
+    const dropdownElement = screen.getByText('dropdown');
+    fireEvent.mouseEnter(dropdownElement);
+    await waitFor(() => {
+      expect(screen.queryByText('drop1')).toBeVisible();
+    });
+    fireEvent.mouseLeave(dropdownElement);
+    await waitFor(() => {
+      expect(screen.queryByText('drop1')).not.toBeVisible();
+    });
+  });
 });
