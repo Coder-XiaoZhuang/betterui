002ff62b49e9c9fcff2189bb24981bf249dd43af
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2024-03-11 17:31:08 +0800

    feat：新增AutoComplete组件支持节流效果

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useState, ChangeEvent, ReactElement } from 'react';
+import React, { FC, useState, ChangeEvent, ReactElement, useEffect } from 'react';
 import Input, { InputProps } from '../Input/input';
 import Icon from '../Icon/icon';
+import useDebounce from '../../hooks/useDebounce';
 
 interface DataSourceObject {
   value: string;
@@ -14,14 +15,13 @@ export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
 
 export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const { fetchSuggestions, onSelect, value, renderOption, ...restProps } = props;
-  const [inputValue, setInputValue] = useState(value);
+  const [inputValue, setInputValue] = useState(value as string);
   const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
   const [ loading, setLoading ] = useState(false);
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim();
-    setInputValue(value);
-    if (value) {
-      const results = fetchSuggestions(value);
+  const debounceValue = useDebounce(inputValue, 500);
+  useEffect(() => {
+    if (debounceValue) {
+      const results = fetchSuggestions(debounceValue);
       if (results instanceof Promise) {
         setLoading(true);
         results.then(data => {
@@ -34,6 +34,10 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     } else {
       setSuggestions([]);
     }
+  }, [debounceValue, fetchSuggestions]);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setInputValue(value);
   };
   const handleSelect = (item: DataSourceType) => {
     setInputValue(item.value);
