a914b1139d60e189ccb95699a18ae4870994964b
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2024-03-18 16:39:29 +0800

    update：完善Upload组件生命周期

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -3,13 +3,29 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Upload } from './upload';
 
+const checkFileSize = (file: File) => {
+  if (Math.round(file.size / 1024) > 50) {
+    alert('file too big');
+    return false;
+  }
+  return true;
+}
+const filePromise =(file: File) => {
+  const newFile = new File([file], 'new_name.docx', { type: file.type });
+  return Promise.resolve(newFile);
+}
+
 const SimpleUpload = () => {
   return (
+    // <Upload 
+    //   action="https://jsonplaceholder.typicode.com/posts"
+    //   onChange={ action('changed') }
+    //   beforeUpload={ checkFileSize }
+    // />
     <Upload 
-      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-      onProgress={ action('progress') }
-      onSuccess={ action('success') }
-      onError={ action('error') }
+      action="https://jsonplaceholder.typicode.com/posts"
+      onChange={ action('changed') }
+      beforeUpload={ filePromise }
     />
   );
 }
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -4,17 +4,21 @@ import Button from '../Button/button';
 
 export interface UploadProps {
   action: string;
+  beforeUpload? : (file: File) => boolean | Promise<File>;
   onProgress?: (percentage: number, file: File) => void;
   onSuccess?: (data: any, file: File) => void;
   onError?: (err: any, file: File) => void;
+  onChange?: (file: File) => void;
 };
 
 export const Upload: FC<UploadProps> = (props) => {
   const {
     action,
+    beforeUpload,
     onProgress,
     onSuccess,
-    onError
+    onError,
+    onChange,
   } = props;
   const fileInput = useRef<HTMLInputElement>(null);
   const handleClick = () => {
@@ -35,31 +39,51 @@ export const Upload: FC<UploadProps> = (props) => {
   const uploadFiles = (files: FileList) => {
     const postFiles = Array.from(files);
     postFiles.forEach(file => {
-      const formData = new FormData();
-      formData.append(file.name, file);
-      axios.post(action, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        onUploadProgress: (e) => {
-          const percentage = Math.round((e.loaded * 100) / (e.total ?? 1)) || 0;
-          if (percentage < 100) {
-            if (onProgress) {
-              onProgress(percentage, file);
-            }
-          }
+      if (!beforeUpload) {
+        post(file);
+      } else {
+        const result = beforeUpload(file);
+        if (result && result instanceof Promise) {
+          result.then(processedFile => {
+            post(processedFile);
+          });
+        } else if (result !== false) {
+          post(file);
         }
-      }).then(res => {
-        if (onSuccess) {
-          onSuccess(res.data, file);
-        }
-      }).catch(err => {
-        if (onError) {
-          onError(err, file);
-        }
-      });
+      }
     });
   };
+  const post = (file: File) => {
+    const formData = new FormData();
+    formData.append(file.name, file);
+    axios.post(action, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      onUploadProgress: (e) => {
+        const percentage = Math.round((e.loaded * 100) / (e.total ?? 1)) || 0;
+        if (percentage < 100) {
+          if (onProgress) {
+            onProgress(percentage, file);
+          }
+        }
+      }
+    }).then(res => {
+      if (onSuccess) {
+        onSuccess(res.data, file);
+      }
+      if (onChange) {
+        onChange(file);
+      }
+    }).catch(err => {
+      if (onError) {
+        onError(err, file);
+      }
+      if (onChange) {
+        onChange(file);
+      }
+    });
+  }
   return (
     <div className='better-upload-component'>
       <Button btnType='primary' onClick={ handleClick }>点击上传</Button>
