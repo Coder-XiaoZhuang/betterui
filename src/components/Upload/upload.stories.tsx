771168068557de5de66d9eb08157f456c66866df
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2024-03-21 16:31:35 +0800

    update：更新Upload组件storybook

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -1,85 +1,64 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { Upload, UploadFile } from './upload';
+import React  from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { Upload, UploadProps } from './upload';
 import Button from '../Button/button';
 import Icon from '../Icon/icon';
+import { JSX } from 'react/jsx-runtime';
 
-const defaultFileList: UploadFile[] = [
-  { uid: '123', name: 'hello.md', size: 1234, status: 'uploading', percent: 30 },
-  { uid: '122', name: 'xyz.md', size: 1234, status: 'success', percent: 30 },
-  { uid: '121', name: 'eyiha.md', size: 1234, status: 'error', percent: 30 },
-];
-// const checkFileSize = (file: File) => {
-//   if (Math.round(file.size / 1024) > 50) {
-//     alert('file too big');
-//     return false;
-//   }
-//   return true;
-// }
-// const filePromise =(file: File) => {
-//   const newFile = new File([file], 'new_name.docx', { type: file.type });
-//   return Promise.resolve(newFile);
-// }
+export default { 
+  title: 'Upload 上传',
+  id: 'Upload',
+  component: Upload,
+  parameters: {
+    docs: {
+      source: {
+        type: "code",
+      },
+    },
+  },
+} as ComponentMeta<typeof Upload>;
 
-const SimpleUpload = () => {
+export const SimpleUpload = (args: JSX.IntrinsicAttributes & UploadProps) => (
+  <Upload
+    {...args}
+    action="https://jsonplaceholder.typicode.com/posts"
+  >
+    <Button size="lg" btnType="primary"><Icon icon="upload" />点击上传</Button>
+  </Upload>  
+);
+SimpleUpload.storyName = '基本上传';
+
+export const CheckUpload = (args: JSX.IntrinsicAttributes & UploadProps) => {
+  const checkFileSize = (file: File) => {
+    if (Math.round(file.size / 1024) > 50) {      
+      alert('上传文件不能大于50Kb！');
+      return false;
+    }
+    return true;
+  };
   return (
-    // <Upload 
-    //   action="https://jsonplaceholder.typicode.com/posts"
-    //   onChange={ action('changed') }
-    //   beforeUpload={ checkFileSize }
-    // />
-    // <Upload 
-    //   action="https://jsonplaceholder.typicode.com/posts"
-    //   onChange={ action('changed') }
-    //   beforeUpload={ filePromise }
-    // />
-    // <Upload 
-    //   action="https://jsonplaceholder.typicode.com/posts"
-    //   onChange={ action('changed') }
-    //   defaultFileList={ defaultFileList }
-    //   onRemove={ action('removed') }
-    // />
-    // <Upload
-    //   action='https://jsonplaceholder.typicode.com/posts'
-    //   onChange={ action('changed') }
-    //   defaultFileList={ defaultFileList }
-    //   onRemove={ action('removed') }
-    //   name='fileName'
-    //   data={{ 'key': 'value' }}
-    //   headers={{ 'X-Powered-By': 'betterui' }}
-    //   accept='.jpg'
-    //   multiple
-    // />
-    // <Upload
-    //   action='https://jsonplaceholder.typicode.com/posts'
-    //   onChange={ action('changed') }
-    //   defaultFileList={ defaultFileList }
-    //   onRemove={ action('removed') }
-    //   name='fileName'
-    //   data={{ 'key': 'value' }}
-    //   headers={{ 'X-Powered-By': 'betterui' }}
-    //   accept='.jpg'
-    //   multiple
-    // >
-    //   <Button btnType="primary">点击上传</Button>
-    // </Upload>
     <Upload
-      action='https://jsonplaceholder.typicode.com/posts'
-      onChange={ action('changed') }
-      onRemove={ action('removed') }
-      name='fileName'
-      multiple
-      drag
+      {...args}
+      action="https://jsonplaceholder.typicode.com/posts"
+      beforeUpload={ checkFileSize }
     >
-      <Icon icon="upload" size="5x" theme="secondary" />
-      <br />
-      <p>点击上传 / 拖拽文件到此区域上传</p>
-    </Upload>
-  );
+      <Button size="lg" btnType="primary"><Icon icon="upload" />不能传大于50Kb！</Button>
+    </Upload>  
+  )
 }
+CheckUpload.storyName = '限制上传';
 
-storiesOf('Upload', module)
-  .add('Simple Upload', () => (
-    <SimpleUpload />
-  ));
+export const DragUpload = (args: JSX.IntrinsicAttributes & UploadProps) => (
+  <Upload
+    {...args}
+    action="https://jsonplaceholder.typicode.com/posts"
+    name="fileName"
+    multiple
+    drag
+  >
+    <Icon icon="upload" size="5x" theme="secondary" />
+    <br/>
+    <p>点击上传 / 拖拽文件到此区域上传</p>
+  </Upload>
+);
+DragUpload.storyName = '拖拽上传';
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -15,24 +15,50 @@ export interface UploadFile {
   error?: any;
 };
 export interface UploadProps {
+  /**必填, 上传的地址 */
   action: string;
+  /**选填，上传的文件列表 */
   defaultFileList?: UploadFile[];
+  /**选填，上传文件之前的钩子，参数为上传的文件，若返回 false 或者 Promise 则停止上传 */
   beforeUpload? : (file: File) => boolean | Promise<File>;
+  /**选填，文件上传时的钩子 */
   onProgress?: (percentage: number, file: UploadFile) => void;
+  /**选填，文件上传成功时的钩子 */
   onSuccess?: (data: any, file: UploadFile) => void;
+  /**选填，文件上传失败时的钩子 */
   onError?: (err: any, file: UploadFile) => void;
+  /**选填，文件状态改变时的钩子，上传成功或者失败时都会被调用	 */
   onChange?: (file: UploadFile) => void;
+  /**选填，文件列表移除文件时的钩子 */
   onRemove?: (file: UploadFile) => void;
+  /**选填，设置上传的请求头部 */
   headers?: {[key: string]: any};
+  /**选填，上传的文件字段名 */
   name?: string;
+  /**选填，上传时附带的额外参数 */
   data?: {[key: string]: any};
+  /**选填，是否支持发送 cookie 凭证信息 */
   withCredentials?: boolean;
+  /**选填，可选参数, 接受上传的文件类型 */
   accept?: string;
+  /**选填，是否支持多选文件，默认不支持 */
   multiple?: boolean;
+  /**设置 Upload 的子元素 */
   children?: ReactNode;
+  /**选填，是否支持拖拽上传，默认为否 */
   drag?: boolean;
 };
 
+
+/**
+ * 通过点击或者拖拽上传文件。
+ * 
+ * ~~~js
+ * // 这样引用
+ * import { Upload } from 'betterui';
+ * ~~~
+ * 
+ */
 export const Upload: FC<UploadProps> = (props) => {
   const {
     action,
