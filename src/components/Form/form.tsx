368561cd0b8f1da856110ae8e26db595e4d53314
zhuangjiaqing <zhuangjiaqing@linklogis.com>
2024-03-25 16:06:11 +0800

    update：更新表单store中的数据

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -5,12 +5,12 @@ export interface FormProps {
   name?: string;
   children?: ReactNode;
 };
-export type IFormContext = Pick<ReturnType<typeof useStore>, 'dispatch'>;
+export type IFormContext = Pick<ReturnType<typeof useStore>, 'dispatch' | 'fields'>;
 export const FormContext = createContext<IFormContext>({} as IFormContext);
 export const Form: FC<FormProps> = (props) => {
   const { name, children } = props;
   const { form, fields, dispatch } = useStore();
-  const passedContext: IFormContext = { dispatch };
+  const passedContext: IFormContext = { dispatch, fields };
   return (
     <>
       <form name={ name } className='better-form'>
--- src/components/Form/formItem.tsx
+++ src/components/Form/formItem.tsx
@@ -10,7 +10,7 @@ export interface FormItemProps {
 
 export const FormItem: FC<FormItemProps> = (props) => {
   const { name, label, children } = props;
-  const { dispatch } = useContext(FormContext);
+  const { dispatch, fields } = useContext(FormContext);
   const rowClass = classNames('better-row', {
     'better-row-no-label': !label,
   });
@@ -21,9 +21,29 @@ export const FormItem: FC<FormItemProps> = (props) => {
       value: {
         label,
         name,
+        value: '',
       },
     });
   }, [dispatch, label, name]);
+  const onValueUpdate = (e: any) => {
+    const value = e.target.value;
+    console.log('value:', value);
+    dispatch({
+      type: 'updateValue',
+      name,
+      value,
+    });
+  }
+  const fieldState = fields[name];
+  const value = fieldState && fieldState.value;
+
+  const controlProps: Record<string, any> = {};
+  controlProps.value = value;
+  controlProps.onChange = onValueUpdate;
+
+  const childList = React.Children.toArray(children);
+  const child = childList[0] as React.ReactElement;
+  const returnChildNode = React.cloneElement(child, { ...child.props, ...controlProps });
   return (
     <div className={ rowClass }>
       { label && 
@@ -32,7 +52,7 @@ export const FormItem: FC<FormItemProps> = (props) => {
         </div>
       }
       <div className='better-form-item'>
-        { children }
+        { returnChildNode }
       </div>
     </div>
   );
